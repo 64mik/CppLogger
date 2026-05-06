a0a558638584ee26423a58edda938d530eba1f10
64mik-dev <64mik.dev@gmail.com>
2026-05-06 12:50:08 +0900

    build: add CMake build system

--- h/mlog.h
+++ h/mlog.h
@@ -1,7 +1,7 @@
 #ifndef MLOG_H
 #define MLOG_H
 #include "logger.h"
-#define MLOG MLOG_INFO 
+#define MLOG MLOG_INFO //추후 콜백으로 변경 예정
 #define MLOG_INFO mik64::Logger::Wrapper(mik64::Logger::LogLevel::L_INFO, __FUNCTION__, __LINE__)
 #define MLOG_WARNING mik64::Logger::Wrapper(mik64::Logger::LogLevel::L_WARNING, __FUNCTION__, __LINE__)
 #define MLOG_ERROR mik64::Logger::Wrapper(mik64::Logger::LogLevel::L_ERROR, __FUNCTION__, __LINE__)
